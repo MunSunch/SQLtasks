47c872e7b1ad8746e0164ed72c3d2906fd447671
MunirTheBest <msunchalyaev@gmail.com>
2023-09-07 12:16:50 +0300

    init commit

---
+++ persons.sql
@@ -0,0 +1,8 @@
+create table PERSONS (
+    name text not null,
+    surname text not null,
+    age integer not null,
+    phone_number text not null,
+    city_of_living text not null,
+    primary key(name, surname, age)
+);
